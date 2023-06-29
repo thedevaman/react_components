2d6b5ae5c0b7cb2fa5cd3b54e2fc2bafe41b34ab
unknown <devaman2508@gmail.com>
2023-06-29 08:53:11 +0530

    react component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './User';
+import Class from './Class';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello React!!!</h1>
+     <User/>
+     <Class/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/User.js
@@ -0,0 +1,10 @@
+function User()
+{
+    return(
+        <div>
+        <h1>User Component</h1>
+        <h1>Jsx Component</h1>
+        </div>
+    )
+}
+export default User;
